4b53a6f5e146dfb2b2d9d8af869234d49ad489f8
irina <irinamalaeva@mail.ru>
2021-06-05 23:28:53 +0300

    done

--- src/Unit3Task5.java
+++ src/Unit3Task5.java
@@ -1,3 +1,5 @@
+
+
 import java.util.Random;
 
 public class Unit3Task5 {
@@ -9,7 +11,7 @@ public class Unit3Task5 {
         double  sum2 = 0;
         for (int i = 0; i < array1.length; i++ ){
             array1[i] = random.nextInt(15);
-           sum1 = sum1 + array1[i];
+            sum1 = sum1 + array1[i];
             System.out.print( array1[i] + " ");
         }
         double average1 = sum1 / 5;
@@ -24,6 +26,22 @@ public class Unit3Task5 {
         }
         double average2 = sum2 / 5;
         System.out.println( "\n" + "average2 = " + average2);
+        boolean compare1 =  (average1 < average2);
+
+        boolean compare2 =  (average1 > average2);
+
+        boolean compare3 =  (average1 == average2);
+
+        if (compare1){
+            System.out.println(compare1 + ";  " + average2 +" more than  "+average1);
+        }
+        if (compare2){
+            System.out.println(compare2 + ";  " + average1 +" more than  "+average2 );
+        }
+        if (compare3){
+            System.out.println(compare3 + "; average2= " + average2 +" equal = " + "average1" + average1 );
+        }
+
     }
 
 }
